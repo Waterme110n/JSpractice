532c3fdc67e3e1f168e31598d01ec606fad6a154
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-08 17:18:29 +0300

    place to snake

--- Snake/index.js
+++ Snake/index.js
@@ -1,1 +1,15 @@
-1231
+var square = document.getElementById('square');
+
+function optimaseSquareWindow() {
+  square.classList.remove('squareV')
+  square.classList.remove('squareh')
+
+  if (window.innerHeight > window.innerWidth)
+    square.classList.add('squareV')
+
+  else
+    square.classList.add('squareH')
+}
+
+optimaseSquareWindow();
+window.addEventListener('resize', optimaseSquareWindow)
