860958ea5fe3e6bf6820cb46486f6c3a8c945b86
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-08 18:06:11 +0300

    movement to snake

--- Snake/index.js
+++ Snake/index.js
@@ -1,8 +1,9 @@
+/*square*/
 var square = document.getElementById('square');
 
 function optimaseSquareWindow() {
   square.classList.remove('squareV')
-  square.classList.remove('squareh')
+  square.classList.remove('squareH')
 
   if (window.innerHeight > window.innerWidth)
     square.classList.add('squareV')
@@ -13,3 +14,49 @@ function optimaseSquareWindow() {
 
 optimaseSquareWindow();
 window.addEventListener('resize', optimaseSquareWindow)
+
+/*snake*/
+var snake = document.getElementById('snake');
+let snakeSpeed = 1000
+let movement = 'left'
+
+function placeSnake() {
+  snake.style.left = (window.innerWidth / 2) + 'px';
+  snake.style.top = (window.innerHeight / 2) + 'px';
+}
+
+function moveSnake() {
+  let currentLeft = parseInt(snake.style.left) || 0;
+  let currentTop = parseInt(snake.style.top) || 0;
+  switch (movement) {
+    case 'left':
+      snake.style.left = (currentLeft - 10) + 'px';
+      break;
+    case 'right': snake.style.left = (currentLeft + 10) + 'px';
+      break;
+    case 'top': snake.style.top = (currentTop - 10) + 'px';
+      break;
+    case 'bottom': snake.style.top = (currentTop + 10) + 'px';
+      break;
+  }
+}
+
+document.addEventListener('keydown', e => {
+  switch (e.key) {
+    case 'ArrowUp':
+      movement = 'top'
+      break;
+    case 'ArrowDown':
+      movement = 'bottom'
+      break;
+    case 'ArrowLeft':
+      movement = 'left'
+      break;
+    case 'ArrowRight':
+      movement = 'right'
+      break;
+  }
+})
+
+placeSnake()
+setInterval(moveSnake, snakeSpeed)
