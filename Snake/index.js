f48e8d70ba9f0557b442e4bf60a8ac099a9c53d2
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-11 15:25:48 +0300

    cell razdel

--- Snake/index.js
+++ Snake/index.js
@@ -1,44 +1,66 @@
 /*square*/
-var square = document.getElementById('square');
+const board = document.getElementById('square');
+var boardSize = 10
 
-function optimaseSquareWindow() {
-  square.classList.remove('squareV')
-  square.classList.remove('squareH')
-
-  if (window.innerHeight > window.innerWidth)
-    square.classList.add('squareV')
-
-  else
-    square.classList.add('squareH')
+function createboard() {
+  board.innerHTML = '';
+  for (let y = 0; y < boardSize; y++) {
+    for (let x = 0; x < boardSize; x++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      board.appendChild(cell);
+    }
+  }
 }
-
-optimaseSquareWindow();
-window.addEventListener('resize', optimaseSquareWindow)
+createboard()
 
 /*snake*/
 var snake = document.getElementById('snake');
-let snakeSpeed = 1000
-let movement = 'left'
+let snakeSpeed = 10
+let movement = 'bottom'
+let nextmovement = '';
+
 
 function placeSnake() {
-  snake.style.left = (window.innerWidth / 2) + 'px';
-  snake.style.top = (window.innerHeight / 2) + 'px';
+  let positionSnake = board.children[55].getBoundingClientRect();
+  snake.style.left = positionSnake.left + 3 + 'px';
+  snake.style.top = positionSnake.top + 3 + 'px';
+}
+
+function checkCell() {
+  for (let i = 0; i < board.children.length; i++) {
+    const cellRect = board.children[i].getBoundingClientRect();
+    const snakeRect = snake.getBoundingClientRect();
+    if (snakeRect.left >= (cellRect.left + 3) && snakeRect.right < (cellRect.right - 3) &&
+      snakeRect.top >= (cellRect.top + 3) && snakeRect.bottom < (cellRect.bottom - 3)) {
+      console.log(i);
+      return i;
+    }
+  }
+  return null
 }
 
 function moveSnake() {
   let currentLeft = parseInt(snake.style.left) || 0;
   let currentTop = parseInt(snake.style.top) || 0;
-  switch (movement) {
+  if (checkCell() !== null) {
+    nextmovement = movement;
+  }
+  switch (nextmovement) {
     case 'left':
-      snake.style.left = (currentLeft - 10) + 'px';
+      snake.style.left = (currentLeft - 1) + 'px';
       break;
-    case 'right': snake.style.left = (currentLeft + 10) + 'px';
+    case 'right':
+      snake.style.left = (currentLeft + 1) + 'px';
       break;
-    case 'top': snake.style.top = (currentTop - 10) + 'px';
+    case 'top':
+      snake.style.top = (currentTop - 1) + 'px';
       break;
-    case 'bottom': snake.style.top = (currentTop + 10) + 'px';
+    case 'bottom':
+      snake.style.top = (currentTop + 1) + 'px';
       break;
   }
+
 }
 
 document.addEventListener('keydown', e => {
@@ -60,3 +82,8 @@ document.addEventListener('keydown', e => {
 
 placeSnake()
 setInterval(moveSnake, snakeSpeed)
+
+/*сделать сетку по которым катается змейка*/
+/*добавить ограничения, по краям и по направлению */
+/*добавить яблочки*/
+/*добавить сложности и конец игры*/
