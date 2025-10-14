6ef38161dcf96b5211c14a5fa80456dba0b5c198
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-14 17:38:13 +0300

    add small mark

--- Clock/index.js
+++ Clock/index.js
@@ -15,16 +15,33 @@ function clockInit() {
   body.appendChild(center);
 
   for (i = 0; i < 12; i++) {
+
     const bigSech = document.createElement('div');
     bigSech.classList.add('bigSech');
 
-    const posHeightToSech = (center.clientHeight - 40) / 2; //40 lenght heigth sech
-    bigSech.style.top = `${posHeightToSech}px`;
-    
-    bigSech.style.transform = `rotate(${i * 30}deg) translate(0,240px) `;
+    const posHeightToBigSech = (center.clientHeight - 40) / 2; //40 lenght heigth sech
+    const posWidghtToBigSech = (center.clientHeight - 6) / 2; 
+    bigSech.style.top = `${posHeightToBigSech}px`;
+    bigSech.style.left = `${posWidghtToBigSech}px`;
+
+    bigSech.style.transform = `rotate(${i * 30}deg) translate(0,235px) `;
     center.appendChild(bigSech);
   }
 
+  for (i = 0; i < 72; i++) {
+    if (i % 6 !== 0) {
+      const smallSech = document.createElement('div');
+      smallSech.classList.add('smallSech');
+
+      const posHeightToSmallSech = (center.clientHeight - 20) / 2; //20 lenght heigth sech
+      const posWidghtToSmallSech = (center.clientHeight - 4) / 2; 
+      smallSech.style.top = `${posHeightToSmallSech}px`;
+      smallSech.style.left = `${posWidghtToSmallSech}px`;
+
+      smallSech.style.transform = `rotate(${i * 5}deg) translate(0,245px) `;
+      center.appendChild(smallSech);
+    }
+  }
 }
 
 clockInit();
