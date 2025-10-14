09e618f8d5bb4ec7ba391b576dbe753b06a833e8
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-14 18:37:46 +0300

    fix minutes 4 not 5 between 2 marks

--- Clock/index.js
+++ Clock/index.js
@@ -1,18 +1,19 @@
 const clock = document.querySelector('.clock');
 const body = document.querySelector('.body');
+const center = document.createElement('div');
+const hourArrow = document.createElement('div');
+const minuteArrow = document.createElement('div');
+const secundArrow = document.createElement('div');
 
 function clockInit() {
-  clockCord = clock.getBoundingClientRect();
-  console.log(clockCord)
-  centerWidht = (clockCord.right - clockCord.left) / 2 + clockCord.left - 5;
-  centerHeight = (clockCord.bottom - clockCord.top) / 2 + clockCord.top - 5; // 5 width and h8 lenth
+  centerWidht = clock.clientWidth / 2 - 5;
+  centerHeight = clock.clientHeight / 2 - 5; // 5 width and h8 lenth
 
-  const center = document.createElement('div');
   center.classList.add('centerCircle');
   center.style.left = `${centerWidht}px`;
   center.style.top = `${centerHeight}px`;
 
-  body.appendChild(center);
+  clock.appendChild(center);
 
   for (i = 0; i < 12; i++) {
 
@@ -20,7 +21,7 @@ function clockInit() {
     bigSech.classList.add('bigSech');
 
     const posHeightToBigSech = (center.clientHeight - 40) / 2; //40 lenght heigth sech
-    const posWidghtToBigSech = (center.clientHeight - 6) / 2; 
+    const posWidghtToBigSech = (center.clientHeight - 6) / 2;
     bigSech.style.top = `${posHeightToBigSech}px`;
     bigSech.style.left = `${posWidghtToBigSech}px`;
 
@@ -28,20 +29,41 @@ function clockInit() {
     center.appendChild(bigSech);
   }
 
-  for (i = 0; i < 72; i++) {
-    if (i % 6 !== 0) {
+  for (i = 0; i < 60; i++) {
+    if (i % 5 !== 0) {
       const smallSech = document.createElement('div');
       smallSech.classList.add('smallSech');
 
       const posHeightToSmallSech = (center.clientHeight - 20) / 2; //20 lenght heigth sech
-      const posWidghtToSmallSech = (center.clientHeight - 4) / 2; 
+      const posWidghtToSmallSech = (center.clientHeight - 4) / 2;
       smallSech.style.top = `${posHeightToSmallSech}px`;
       smallSech.style.left = `${posWidghtToSmallSech}px`;
 
-      smallSech.style.transform = `rotate(${i * 5}deg) translate(0,245px) `;
+      smallSech.style.transform = `rotate(${i * 6}deg) translate(0,245px) `;
       center.appendChild(smallSech);
     }
   }
 }
 
+
+function ArrowInit() {
+  hourArrow.classList.add('hour');
+  hourArrow.style.top = '5px'
+  center.appendChild(hourArrow);
+
+  minuteArrow.classList.add('minute'); 
+  minuteArrow.style.left = '1px' // (10-8)/2
+  minuteArrow.style.top = '5px'
+  center.appendChild(minuteArrow);
+
+  secundArrow.classList.add('second');
+  secundArrow.style.left = '4px' // (10-8)/2
+  secundArrow.style.top = '5px'
+  center.appendChild(secundArrow);
+  
+}
+
+
+
 clockInit();
+ArrowInit();
