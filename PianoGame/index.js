8fbda999a32cd8c5b503ab272da7801e1a75a26b
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-15 16:15:50 +0300

    add press keys on keyboard to note

--- PianoGame/index.js
+++ PianoGame/index.js
@@ -1,4 +1,6 @@
 const body = document.querySelector('.body');
+const buttons = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+let char = -1;
 
 function initButtons() {
     for (let i = 0; i < 7; i++) {
@@ -10,15 +12,24 @@ function initButtons() {
 
 function playNote() {
     let notes = document.querySelectorAll('.note')
-    for (let j = 1; j < notes.length+1; j++) {
-        notes[j-1].addEventListener("click", e => {
+    for (let j = 1; j < notes.length + 1; j++) {
+        notes[j - 1].addEventListener('click', e => {
             let audio = new Audio(`notes/${j}.mp3`);
             audio.play();
         })
     }
 }
 
+function PressNote() {
+    document.addEventListener('keydown', e => {
+        char = buttons.indexOf(e.key);
+        if (char != -1) {
+            let audio = new Audio(`notes/${char + 1}.mp3`);
+            audio.play();
+        }
+    })
+}
 
-
+PressNote()
 initButtons();
 playNote();
