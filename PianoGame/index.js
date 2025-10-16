41734a06e37f1649d08da8076dcda432ea433280
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-16 15:28:42 +0300

    added stop/start button in menu, status p,

--- PianoGame/index.js
+++ PianoGame/index.js
@@ -4,7 +4,10 @@ const pianino = document.querySelector('.pianino');
 let charNumber = -1;
 let Song = document.getElementById('dificulty');
 let start = document.querySelector('.startgame');
+let statusP = document.querySelector('.status')
 let notes = [];
+let playTimeouts = [];
+let dropDowns;
 const happybithday = [
     [0, 500], [0, 500], [1, 1000], [0, 1000], [3, 1000], [2, 1000],
     [0, 500], [0, 500], [1, 1000], [0, 1000], [4, 1000], [3, 1000],
@@ -57,13 +60,14 @@ function PlaySong(songArr) {
 
         totalDelay += delay;
 
-        setTimeout(() => {
+        playTimeout = setTimeout(() => {
             let dropElement = document.createElement('div');
             dropElement.classList.add('dropElement');
 
             const notesPosition = notes[noteIndex].getBoundingClientRect();
-            dropElement.style.left = notesPosition.left + 'px';
+            dropElement.style.left = notesPosition.left + 10 + 'px';
             dropElement.style.top = notesPosition.top + 'px';
+            dropElement.style.width = notesPosition.width - 20 + 'px';
             dropElement.style.transform = 'translateY(-1000px)';
 
             body.appendChild(dropElement);
@@ -71,7 +75,21 @@ function PlaySong(songArr) {
 
             dropElement.style.transform = 'translateY(1000px)';
 
+            if (i == songArr.length - 1) {
+                setTimeout(() => {
+                    start.style.backgroundColor = '#dbe2ef';
+                    start.textContent = 'Start';
+                    statusP.textContent = 'Choоse your level';
+                    dropDowns = document.querySelectorAll('.dropElement');
+                    dropDowns.forEach(el => {
+                        el.remove();
+                    })
+                }, 7000);
+            }
+
         }, totalDelay);
+        playTimeouts.push(playTimeout);
+
     }
 }
 
@@ -81,7 +99,6 @@ function chooseSong() {
 
     switch (Song.value) {
         case 'free': {
-            //do nothing Clear all window
             break;
         }
         case 'hb': {
@@ -93,11 +110,29 @@ function chooseSong() {
 
 initButtons();
 playNote();
-start.addEventListener('click', chooseSong);
+
+start.addEventListener('click', e => {
+    let button = document.querySelector('.startgame');
+    if (button.textContent == 'Start') {
+        button.style.backgroundColor = 'red';
+        button.textContent = 'Stop';
+        statusP.textContent = 'Playing...';
+        chooseSong();
+    } else {
+        button.style.backgroundColor = '#dbe2ef';
+        button.textContent = 'Start';
+        statusP.textContent = 'Choоse your level';
+        dropDowns = document.querySelectorAll('.dropElement');
+        dropDowns.forEach(el => {
+            el.remove();
+        })
+        playTimeouts.forEach(playTimeout => {
+            clearTimeout(playTimeout);
+        });
+    }
+});
 
 //можно сделать чтобы ловились летящие кубики + pointы
-//доделать меню на stop/start меняется text
-
-
+//доделать меню на кнопку динамичную stop/start меняется text
 
 
