c08387b60517b606cc221584fb4e79739644fbdd
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-20 19:36:11 +0300

    add NastyaClick

--- MoleGame/index.js
+++ MoleGame/index.js
@@ -1,31 +1,40 @@
 let body = document.body;
 let pits = document.querySelectorAll('.pit');
 let nastya = document.querySelector('.nastya');
+let points = 0;
+let nastyaInterval;
 
 function showNastya() {
   let where = Math.ceil(Math.random() * 3);
-  console.log(where);
   let pos = pits[where - 1].getBoundingClientRect();
   nastya.style.left = pos.left + 70 + 'px';
   nastya.style.top = pos.top + 10 + 'px'
   nastya.style.display = 'block';
   moveNastya();
+  console.log(points)
 }
 
 function moveNastya() {
   let speed = Math.ceil(Math.random() * 50);
   let nastyaPos = nastya.getBoundingClientRect();
-  const max = nastyaPos.top - nastyaPos.height / 1.5; 
-  let nastyaInterval = setInterval(() => {
+  const max = nastyaPos.top - nastyaPos.height / 1.5;
+  nastyaInterval = setInterval(() => {
     nastyaPos = nastya.getBoundingClientRect();
     if (nastyaPos.top > max) {
       nastya.style.top = nastyaPos.top - 3 + 'px';
     } else {
-      clearInterval(nastyaInterval); 
+      clearInterval(nastyaInterval);
       nastya.style.display = 'none';
-      setTimeout(showNastya, 1000); 
+      setTimeout(showNastya, 1000);
     }
   }, speed);
 }
+
+nastya.addEventListener('click', () => {
+  points += 50
+  clearInterval(nastyaInterval);
+  nastya.style.display = 'none';
+  setTimeout(showNastya, 1000);
+})
 
 showNastya();
