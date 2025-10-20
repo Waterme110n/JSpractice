9127651157591a421fdf50c05ba7b7ad91d6c1af
Pavel Osadcy <pavelasadchy@gmail.com>
2025-10-20 19:29:16 +0300

    add moveNastyTop

--- MoleGame/index.js
+++ MoleGame/index.js
@@ -2,14 +2,30 @@ let body = document.body;
 let pits = document.querySelectorAll('.pit');
 let nastya = document.querySelector('.nastya');
 
-function showNastya(){
-  let where = Math.ceil(Math.random()*3);
+function showNastya() {
+  let where = Math.ceil(Math.random() * 3);
   console.log(where);
-  let pos = pits[where-1].getBoundingClientRect();
+  let pos = pits[where - 1].getBoundingClientRect();
   nastya.style.left = pos.left + 70 + 'px';
-  nastya.style.top = pos.top - 90 + 'px'
+  nastya.style.top = pos.top + 10 + 'px'
   nastya.style.display = 'block';
+  moveNastya();
+}
 
+function moveNastya() {
+  let speed = Math.ceil(Math.random() * 50);
+  let nastyaPos = nastya.getBoundingClientRect();
+  const max = nastyaPos.top - nastyaPos.height / 1.5; 
+  let nastyaInterval = setInterval(() => {
+    nastyaPos = nastya.getBoundingClientRect();
+    if (nastyaPos.top > max) {
+      nastya.style.top = nastyaPos.top - 3 + 'px';
+    } else {
+      clearInterval(nastyaInterval); 
+      nastya.style.display = 'none';
+      setTimeout(showNastya, 1000); 
+    }
+  }, speed);
 }
 
 showNastya();
